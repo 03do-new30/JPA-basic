810191355309274324bbdbcdccc826669fbe5c23
donew <eastgloss0330@naver.com>
2024-03-28 16:47:29 +0900

    [Feat] JPA 조회 수정 예시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,20 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
