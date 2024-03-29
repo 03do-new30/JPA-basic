ccaa9415b1845b16d8136281810dd7f3fcd0ed09
donew <eastgloss0330@naver.com>
2024-03-29 17:47:59 +0900

    [Feat] 영속성 컨텍스트 - 준영속

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,10 +14,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            em.flush(); // 쿼리가 날아가는 시점
+            // 준영속
+            // JPA에서 관리하지 않음, 영속성 컨텍스트에서 관리하지 않음
+            // memeber 관련 모든 것이 영속성 컨텍스트에서 빠짐
+            // update 쿼리가 나가지 않을 것
+            em.detach(member); // member 엔티티만 준영속 상태로 전환
+
+            em.clear(); // 영속성 컨텍스트를 완전히 초기화
+            // 1차 캐시에 없으므로 똑같은 멤버를 조회해도 다시 select 쿼리 나감
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("==========");
 
@@ -25,7 +34,7 @@ public class JpaMain {
         } catch (Exception e) {
             tx.rollback();
         } finally {
-            em.close();
+            em.close(); // 영속성 컨텍스트를 종료
         }
         emf.close();
     }
