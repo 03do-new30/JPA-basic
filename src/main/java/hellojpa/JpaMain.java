b5c33c06e5cd72bfb3de0e0fff824dcc6716c48c
donew <eastgloss0330@naver.com>
2024-03-29 16:25:22 +0900

    [Feat] 영속성 컨텍스트 - 영속 엔티티의 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,6 +19,9 @@ public class JpaMain {
             // 두번째 조회할 때는 캐시에 있는 것을 가져오므로 쿼리가 나오면 안됨
             Member findMember2 = em.find(Member.class, 101L);
 
+            // 영속 엔티티의 동일성 보장
+            System.out.println("result = " + (findMember1 == findMember2));
+
             tx.commit(); // DB에 쿼리가 날아가는 시점
         } catch (Exception e) {
             tx.rollback();
