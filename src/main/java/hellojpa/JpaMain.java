f994836547a3c9c35a6286e2525e099067b9c728
donew <eastgloss0330@naver.com>
2024-03-29 16:21:51 +0900

    [Feat] 영속성 컨텍스트 - 1차 캐시에서 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,13 +16,19 @@ public class JpaMain {
         try {
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             // 영속
             System.out.println("=== BEFORE ===");
             em.persist(member);
             System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+            // 조회를 했는데 Select 쿼리가 안나감?
+            // DB에서 찾아오지 않고, 1차 캐시에서 가져오기 때문!
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
             tx.commit(); // DB에 쿼리가 날아가는 시점
         } catch (Exception e) {
