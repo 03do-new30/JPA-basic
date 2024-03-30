25119f227d1d2bb0b9441f3d8df17181e1ce8d1c
donew <eastgloss0330@naver.com>
2024-03-30 13:27:40 +0900

    [Feat] 엔티티 매핑 - 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,22 +14,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(1L);
+            member.setUsername("A");
+            member.setRoleType(RoleType.ADMIN);
 
-            // 준영속
-            // JPA에서 관리하지 않음, 영속성 컨텍스트에서 관리하지 않음
-            // memeber 관련 모든 것이 영속성 컨텍스트에서 빠짐
-            // update 쿼리가 나가지 않을 것
-            em.detach(member); // member 엔티티만 준영속 상태로 전환
-
-            em.clear(); // 영속성 컨텍스트를 완전히 초기화
-            // 1차 캐시에 없으므로 똑같은 멤버를 조회해도 다시 select 쿼리 나감
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("==========");
-
+            em.persist(member);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,35 +1,96 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
+
     @Id
     private Long id;
-    private String name;
 
-    // JPA를 위한 기본 생성자
-    public Member() {}
+    // 객체에는 username, 컬럼에는 name 이라는 이름을 쓰고 싶을 때
+    @Column(name = "name")
+    private String username;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    private Integer age;
 
-    public long getId() {
+   @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+   @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+   @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+   private LocalDate testLocalDate;
+   private LocalDateTime testLocalDateTime;
+
+   @Lob
+    private String description;
+
+   @Transient
+   private int temp; // DB와 관계 없어. 매핑하지 않을래
+
+   public Member() {}
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
