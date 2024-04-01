ac477c2a25c8808ee1d1dee5082855b41c481f44
donew <eastgloss0330@naver.com>
2024-04-01 20:49:10 +0900

    [Refactor] 다양한 연관관계 매핑 - 일대다 양방향

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -21,6 +21,12 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) // 일대다 양방향
+    // 일대다에서 연관관계의 주인처럼 보이지 않기 위해서 insertable = false, updatable = false
+    // 읽기 전용으로. 업데이트 안하게끔.
+    private Team team;
+
     public Long getId() {
         return id;
     }
