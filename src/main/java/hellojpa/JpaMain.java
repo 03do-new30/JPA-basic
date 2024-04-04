42971379760b2aa8a1b91c62b4dd3d602c0cb30d
donew <eastgloss0330@naver.com>
2024-04-04 22:39:51 +0900

    [Feat] 즉시로딩의 문제점 - N + 1 PROBLEM

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,24 +19,36 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("teamA");
+            em.persist(team1);
+
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
 
             Member member1 = new Member();
             member1.setUsername("member1");
-            member1.setTeam(team);
+            member1.setTeam(team1);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team2);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println("m.getClass() = " + m.getClass());
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-            System.out.println("========================================");
-            System.out.println("m.getTeam().getName() = " + m.getTeam().getName());
-            System.out.println("========================================");
+//            Member m = em.find(Member.class, member1.getId());
+
+            // N + 1 PROBLEM
+            // 즉시 로딩 시, JPQL 쓰면 쿼리가 여러번 나간다
+            // 1. SQL로 변환(SELECT * FROM MEMBER)해서, MEMBER 테이블을 쭉 가져온다
+            // 2. 어라? MEMBER 가져왔더니 Team이 즉시로딩으로 되어있네? 이를 위한 쿼리가 별도로 나감
+            // (SELECT * FROM TEAM WHERE TEAM_ID = team1의 아이디)
+            // (SELECT * FROM TEAM WHERE TEAM_ID = team2의 아이디) ...
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
 
 
             tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
