286d2257b867b561e7c278eb639a287e513572e5
donew <eastgloss0330@naver.com>
2024-04-04 16:13:26 +0900

    [Refactor] 프록시와 연관관계 관리 - em.getReference() 사용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -26,8 +26,11 @@ public class JpaMain {
             em.flush();
             em.clear(); // 영속성 컨텍스트 깔끔하게
 
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("findMember.getId() = " + findMember.getId());
+//            Member findMember = em.find(Member.class, member.getId());
+            Member findMember = em.getReference(Member.class, member.getId()); // 호출 시점에는 쿼리하지 않음
+            System.out.println("findMember = " + findMember.getClass()); // Member$HibernateProxy
+            System.out.println("findMember.getId() = " + findMember.getId()); // 이미 getId를 알기에 쿼리하지 않음
+            // 값이 실제 사용되는 시점에 쿼리함 !!
             System.out.println("findMember.getUsername() = " + findMember.getUsername());
 
             tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
