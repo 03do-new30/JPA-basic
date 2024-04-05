eb0bc5ec2056f3b181f274f8b5930b1c23a81273
donew <eastgloss0330@naver.com>
2024-04-05 13:28:24 +0900

    [Feat] Member 필드 수정 - 임베디드타입 적용 전

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,22 +18,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0); // 첫번째 자식을 지워보자
-            // orphanRemoval = true이기 때문에
-            // childList 컬렉션에서 빠진 자식은 DB에서 삭제(DELETE)
 
             tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,11 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", // 매핑할 DB 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
-public class Member extends BaseEntity {
+public class Member {
 
     @Id
     @GeneratedValue
@@ -23,37 +19,12 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    // 기간
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 
-    // 모든 엔티티의 공통 속성 -> BaseEntity로
-//    private String createdBy;
-//    private LocalDateTime createdDate;
-//    private String lastModifiedBy;
-//    private LocalDateTime lastModifiedDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
+    // 주소
+    private String city;
+    private String street;
+    private String zipcode;
 }
