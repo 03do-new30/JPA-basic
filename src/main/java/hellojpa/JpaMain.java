7f2b8e4a48ebcdc6ffbafde04b7e796fea13f464
donew <eastgloss0330@naver.com>
2024-04-05 15:20:01 +0900

    [Feat] 값 타입 컬렉션 대안 - Entity로 승격

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -28,8 +28,8 @@ public class JpaMain {
             member.getFavoriteFoods().add("족발");
             member.getFavoriteFoods().add("피자");
 
-            member.getAddressHistory().add(new Address("old1", "street", "10000"));
-            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
 
             // 값 타입 컬렉션의 라이프사이클이 멤버에 의존
             em.persist(member);
@@ -38,28 +38,7 @@ public class JpaMain {
             em.clear();
 
             System.out.println("========== START ==========");
-            // 값 타입 컬렉션들은 지연 로딩
-            Member findMember = em.find(Member.class, member.getId()); // Member에서만 조회 - 지연 로딩
-
-            // homeCity -> newCity
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            // 값 타입 컬렉션 수정
-            // 치킨 -> 한식
-            // String 자체가 값타입. 업데이트라는게 없고 갈아끼워야함
-            findMember.getFavoriteFoods().remove("치킨"); // DELETE query
-            findMember.getFavoriteFoods().add("한식"); // INSERT query
-
-            // old1 -> new1
-            // remove시 equals로 비교해서 같은 인스턴스 지우기때문에
-            // old1과 완전히 똑같은 인스턴스를 만들어주고 지운다
-            // equlas와 hashCode가 제대로 구현되어있어야 한다
-            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-            // 현재 지우는 데이터에 걸려있는 member id 관련된 모든 데이터를 싹 다 지운다
-
-            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
-            // 컬렉션에 최종 남은 데이터들을 insert친다
+            Member findMember = em.find(Member.class, member.getId());
 
             tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -27,10 +27,15 @@ public class Member {
     @Column(name = "FOOD_NAME")
     private Set<String> favoriteFoods = new HashSet<>();
 
-    @ElementCollection
-    @CollectionTable(name = "ADDRESS",
-        joinColumns = @JoinColumn(name = "MEMBER_ID"))
-    private List<Address> addressHistory = new ArrayList<>();
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS",
+//        joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    // 값 타입 컬렉션 대안
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -64,11 +69,11 @@ public class Member {
         this.favoriteFoods = favoriteFoods;
     }
 
-    public List<Address> getAddressHistory() {
+    public List<AddressEntity> getAddressHistory() {
         return addressHistory;
     }
 
-    public void setAddressHistory(List<Address> addressHistory) {
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
         this.addressHistory = addressHistory;
     }
 }
