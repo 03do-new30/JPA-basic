8f733ded811dfe1fecb358366849b6200882f7a5
donew <eastgloss0330@naver.com>
2024-04-05 14:09:15 +0900

    [Feat] 값 타입의 비교는 equals로

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -2,23 +2,19 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
-        // 자바의 기본 타입은 절대 공유되지 않는다
-        // Side Effect가 없다
         int a = 10;
-        int b = a;
+        int b = 10;
+        System.out.println("a == b : " + (a == b)); // true
 
-        a = 20;
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 == address2 : " + (address1 == address2)); // false
 
-        System.out.println("a = " + a); // 20
-        System.out.println("b = " + b); // 10
-
-        // 래퍼 클래스는 reference를 공유는 가능하지만
-        // 이 값을 변경할 방법이 없다
-        Integer aa = new Integer(10);
-        Integer bb = aa;
-//        aa.setValue(20); 같은 방법이 없다
-        // -> Side Effect가 없다
-        System.out.println("aa = " + aa); // 10
-        System.out.println("bb = " + bb); // 10
+        // 값타입은 인스턴스가 달라도 그 안에 값이 같으면 같은 것으로 봐야 한다
+        // 값타입은 equals method를 사용해야 한다
+        // ==는 인스턴스의 참조값을 비교
+        // .eqauls()는 인스턴스의 값을 비교
+        System.out.println("address1.equals(address2) : " + address1.equals(address2)); // true
+        
     }
 }
