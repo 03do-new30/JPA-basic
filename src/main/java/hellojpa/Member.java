650b0eb7554b4cc5a2b2edb3b0b7d1562614060c
donew <eastgloss0330@naver.com>
2024-04-05 13:43:30 +0900

    [Feat] @AttributeOverride: 속성 재정의

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -19,6 +19,13 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
+    // 한 엔티티에서 같은 임베디드 타입을 사용하면 컬럼명 중복 에러 -> @AttributeOverrides, @AttributeOverride로 해결
+    @Embedded
+    @AttributeOverrides({@AttributeOverride(name = "city", column = @Column(name = "work_city")),
+                            @AttributeOverride(name = "street", column = @Column(name = "work_street")),
+                            @AttributeOverride(name = "zipcode", column = @Column(name = "work_zipcode"))})
+    private Address workAddress;
+
     public Long getId() {
         return id;
     }
